d80fdbe19b5d3f1d1980a058efe72fad9be59854
joshuanieh <b07502027@ntu.edu.tw>
2022-07-01 18:58:46 +0800

    init

---
+++ config.py
@@ -0,0 +1,15 @@
+# data prarameters
+concat_nframes = 19             # the number of frames to concat with, n must be odd (total 2k+1 = n frames)
+train_ratio = 0.8               # the ratio of data used for training, the rest will be used for validation
+
+# training parameters
+seed = 0                        # random seed
+batch_size = 512                # batch size
+num_epoch = 10                  # the number of training epoch
+learning_rate = 0.0001           # learning rate
+model_path = './model.ckpt'     # the path where the checkpoint will be saved
+
+# model parameters
+input_dim = 39 * concat_nframes # the input dim of the model, you should not change the value
+hidden_layers = 1               # the number of hidden layers
+hidden_dim = 512                # the hidden dim
---
+++ model.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(BasicBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        x = self.block(x)
+        return x
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super().__init__()
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=1, dropout = 0.25, bidirectional = True, batch_first=True)
+    def forward(self, x):
+        #print(x.shape)
+        x = torch.reshape(x, (1, x.shape[0], x.shape[1]))
+        #print(x.shape)
+        x, y = self.lstm(x, None)
+        #print(x.shape)
+        x = x.squeeze()
+        #print(x.shape)
+        x = torch.reshape(x, (x.shape[0], 2, x.shape[1]//2))
+        #print(x.shape)
+        #print(torch.flip(x[:,1,:], dims=(0,)).shape)
+        x = torch.cat([x[:,0,:], torch.flip(x[:,1,:], dims=(0,))], dim = 1)
+        print(x.shape)
+        #print(y)
+        return x
+
+class Classifier(nn.Module):
+    def __init__(self, input_dim, output_dim=41, hidden_layers=1, hidden_dim=256):
+        super(Classifier, self).__init__()
+        self.fc = nn.Sequential(
+            LSTM(input_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim*2),
+            nn.Sigmoid(),
+            LSTM(hidden_dim*2, hidden_dim),
+            nn.BatchNorm1d(hidden_dim*2),
+            nn.Sigmoid(),
+            LSTM(hidden_dim*2, hidden_dim),
+            nn.BatchNorm1d(hidden_dim*2),
+            nn.Sigmoid(),
+            LSTM(hidden_dim*2, hidden_dim),
+            nn.BatchNorm1d(hidden_dim*2),
+            nn.Sigmoid(),
+            LSTM(hidden_dim*2, hidden_dim),
+            nn.BatchNorm1d(hidden_dim*2),
+            nn.Sigmoid(),
+            nn.Linear(hidden_dim*2, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.Sigmoid(),
+            nn.Linear(hidden_dim, output_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
